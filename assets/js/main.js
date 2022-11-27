b73ef657a0b25086d7485b5ef90f0c6bcc6b2a74
alicanov98 <alicanov1998@gmail.com>
2022-11-27 20:08:25 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// const activePages = window.location.pathname
+// const navLinks = document.querySelectorAll('.nav-list .navLink').forEach(link=>{
+//   if(link.href.includes(`${activePages}`)){
+//     link.classList.add('active')
+//   }
+// })
